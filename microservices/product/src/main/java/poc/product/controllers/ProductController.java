a99b083eaeb0722bf18546b47f0a8520dffcc098
nassimguelai <nassim.guelai@gmail.com>
2024-05-20 21:24:30 +0200

    Initialisation des microservices springboot. Création des microsevices registry, gateway et product. L'ensemble fonctionne localement.

---
+++ microservices/product/src/main/java/poc/product/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package poc.product.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @GetMapping("/all")
+    public String getAllProducts() {
+        return "All products";
+    }
+}
